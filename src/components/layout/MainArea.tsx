333bd6b0be7872740b2767c0b0349fd76e1d3c9b
Ryoh96 <account@kurokiryoh.com>
2023-03-07 17:32:37 +0900

    Fine-tune MainArea padding

--- src/components/layout/MainArea.tsx
+++ src/components/layout/MainArea.tsx
@@ -2,5 +2,9 @@ import styled from 'styled-components'
 
 const MainArea = styled.div`
   flex: 1 1 auto;
+  overflow-x: hidden;
+  background-color: #fff;
+  padding: 40px 34px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 `
 export default MainArea
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -6,6 +6,10 @@ const Base = createGlobalStyle`
     font-size: 16px;
   }
 
+  body {
+    background-color: #383838;
+  }
+
   a {
     text-decoration: none;
   }
