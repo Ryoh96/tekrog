44f0e48eb4e376a6132b4154cc27264f04becc19
Ryoh96 <account@kurokiryoh.com>
2023-03-07 12:37:28 +0900

    Prepared Theme file and GlobalStyle file
    tsconfigにbaseUrlの追加

---
+++ src/theme/gradient.ts
@@ -0,0 +1,5 @@
+const gradient = {
+  main: "linear-gradient(273deg, #038fba, #117caf 41%, #1f2a82)"
+}
+
+export default gradient
---
+++ src/theme/index.ts
@@ -0,0 +1,5 @@
+import gradient from "./gradient"
+
+export const theme = {
+  gradient
+} as const
