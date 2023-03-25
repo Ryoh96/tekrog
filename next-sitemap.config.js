e549cdc48bf2ec4139ee9573498e71fda14373a5
Ryoh96 <account@kurokiryoh.com>
2023-03-25 18:54:54 +0900

    Change setting files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /**@type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
-  outDir: "./dist"
 }
 
 module.exports = config
--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   images: {
     domains: ['xs363422.xsrv.jp', 'www.google.com', 's.wordpress.com'],
   },
-  distDir: "dist"  
 }
 
 module.exports = nextConfig
