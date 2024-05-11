661bf917f4ad14ebb4648437abb4ef577f139114
Ryoh96 <account@kurokiryoh.com>
2024-05-11 22:01:55 +0900

    Modified adsense

--- src/app/_components/libs/GoogleAdsenseScript/index.tsx
+++ src/app/_components/libs/GoogleAdsenseScript/index.tsx
@@ -4,7 +4,7 @@ const GoogleAdsenseScript = () => {
   return (
     <Script
       async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5954645555619996"
       crossOrigin="anonymous"
       strategy="lazyOnload"
     />
