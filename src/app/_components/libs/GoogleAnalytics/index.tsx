00075799d6aad215f20eb34050e844f5e81b455f
Ryoh96 <account@kurokiryoh.com>
2024-05-14 12:45:28 +0900

    modified analytics log

--- src/app/_components/libs/GoogleAnalytics/index.tsx
+++ src/app/_components/libs/GoogleAnalytics/index.tsx
@@ -17,7 +17,6 @@ const GoogleAnalytics = () => {
 
   return (
     <>
-      {console.log("--test-console--")}
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_GA4_ID}`}
